61c8412cf67f398d2cc81b0b2b1db4eedd0e9637
manzitresor <manzitre@gmail.com>
2024-09-29 22:16:43 +0200

    feat: create item interface and findAll method

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -8,12 +8,15 @@ import {
   Body,
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create.item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly ItemsService: ItemsService) {}
   @Get()
-  findAll(): string {
-    return 'Get all items';
+  findAll(): Item[] {
+    return this.ItemsService.findAll();
   }
 
   @Get(':id')
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private readonly Items: Item[] = [
+    {
+      id: '232332',
+      name: 'Item One',
+      qty: 321,
+      description: 'This is item one from here',
+    },
+    {
+      id: '242442',
+      name: 'Item Two',
+      qty: 12,
+      description: 'This is item Two Description',
+    },
+  ];
+  findAll(): Item[] {
+    return this.Items;
+  }
+}
