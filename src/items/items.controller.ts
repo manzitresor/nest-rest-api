7e1bd06e080c92e36e4e64ac83c1479d3486b9c0
manzitresor <manzitre@gmail.com>
2024-09-29 21:30:29 +0200

    feat: create service

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Delete, Put, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Put,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { CreateItemDto } from './dto/create.item.dto';
 
 @Controller('items')
 export class ItemsController {
@@ -7,14 +16,19 @@ export class ItemsController {
     return 'Get all items';
   }
 
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return `Get Your Id: ${id}`;
+  }
+
   @Post()
-  create(): string {
-    return 'Created successfull';
+  create(@Body() createitemDto: CreateItemDto): string {
+    return `Name: ${createitemDto.name} Description: ${createitemDto.description}`;
   }
 
   @Put(':id')
-  update(@Param('id') id: string) {
-    return `Successful updated ${id}`;
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+    return `Updated ${id} With Name: ${updateItemDto.name} Quantinty: ${updateItemDto.qty}`;
   }
 
   @Delete(':id')
